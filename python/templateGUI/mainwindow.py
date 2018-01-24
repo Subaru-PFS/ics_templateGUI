ada61ec7a4cb544296992778c6438ad7d64f6fe9
LE FUR Arnaud <arnaud.lefur@lam.fr>
2018-01-24 09:37:05 +0100

    first commit

---
+++ python/templateGUI/mainwindow.py
@@ -0,0 +1,83 @@
+__author__ = 'alefur'
+
+from PyQt5.QtWidgets import QGridLayout, QWidget, QGroupBox, QLineEdit, QPushButton, QLabel, QPlainTextEdit
+from PyQt5.QtGui import QFont, QTextCursor
+from datetime import datetime as dt
+from widgets import ValueGB
+from functools import partial
+
+class LogArea(QPlainTextEdit):
+    def __init__(self):
+        QPlainTextEdit.__init__(self)
+        self.logArea = QPlainTextEdit()
+        self.setMaximumBlockCount(10000)
+        self.setReadOnly(True)
+
+        self.setStyleSheet("background-color: black;color:white;")
+        self.setFont(QFont("Monospace", 8))
+
+    def newLine(self, line):
+        self.insertPlainText("\n%s  %s" % (dt.now().strftime("%H:%M:%S.%f"), line))
+        #self.moveCursor(QTextCursor.End)
+        #self.ensureCursorVisible()
+    def trick(self, qlineedit):
+        self.newLine(qlineedit.text())
+
+class Device(QGroupBox):
+    def __init__(self, title):
+        QGroupBox.__init__(self)
+        self.setTitle(title)
+        self.grid = QGridLayout()
+
+
+class Example(QWidget):
+    def __init__(self, mainTree):
+        QWidget.__init__(self)
+        self.mainTree = mainTree
+        self.mainLayout = QGridLayout()
+
+        self.mainLayout.addWidget(ValueGB('Enabled ', self.actor.models['xcu_r0'], 'ionpump1', 0, '{:g}'), 0, 0)
+        self.mainLayout.addWidget(ValueGB('Voltage(V)', self.actor.models['xcu_r0'], 'ionpump1', 1, '{:g}'), 0, 1)
+        self.mainLayout.addWidget(ValueGB('Current(A)', self.actor.models['xcu_r0'], 'ionpump1', 2, '{:g}'), 0, 2)
+        self.mainLayout.addWidget(ValueGB('Temperature(K)', self.actor.models['xcu_r0'], 'ionpump1', 3, '{:g}'), 0, 3)
+        self.mainLayout.addWidget(ValueGB('Pressure(Torr)', self.actor.models['xcu_r0'], 'ionpump1', 4, '{:g}'), 0, 4)
+
+        self.mainLayout.addWidget(ValueGB('Enabled ', self.actor.models['xcu_r0'], 'ionpump2', 0, '{:g}'), 1, 0)
+        self.mainLayout.addWidget(ValueGB('Voltage(V)', self.actor.models['xcu_r0'], 'ionpump2', 1, '{:g}'), 1, 1)
+        self.mainLayout.addWidget(ValueGB('Current(A)', self.actor.models['xcu_r0'], 'ionpump2', 2, '{:g}'), 1, 2)
+        self.mainLayout.addWidget(ValueGB('Temperature(K)', self.actor.models['xcu_r0'], 'ionpump2', 3, '{:g}'), 1, 3)
+        self.mainLayout.addWidget(ValueGB('Pressure(Torr)', self.actor.models['xcu_r0'], 'ionpump2', 4, '{:g}'), 1, 4)
+
+        self.mainLayout.addWidget(ValueGB('Cooler_Setpoint(K)', self.actor.models['xcu_r0'], 'coolerTemps', 0, '{:g}'),
+                                  2, 0)
+        self.mainLayout.addWidget(ValueGB('Cooler_Reject(C)', self.actor.models['xcu_r0'], 'coolerTemps', 1, '{:g}'), 2,
+                                  1)
+        self.mainLayout.addWidget(ValueGB('Cooler_Tip(K)', self.actor.models['xcu_r0'], 'coolerTemps', 2, '{:g}'), 2, 2)
+        self.mainLayout.addWidget(ValueGB('Cooler_Power(W)', self.actor.models['xcu_r0'], 'coolerTemps', 3, '{:g}'), 2,
+                                  3)
+
+        self.mainLayout.addWidget(ValueGB('Shutters', self.actor.models['enu'], 'shutters', 2, '{:s}'), 3, 0)
+
+        self.commandLine = QLineEdit()
+        self.commandButton = QPushButton('Send Command')
+        self.commandButton.clicked.connect(self.sendCommand)
+
+        self.logArea = LogArea()
+        self.actor.dummyLab = QLineEdit()
+        self.actor.dummyLab.textChanged.connect(partial(self.logArea.trick, self.actor.dummyLab))
+
+        self.mainLayout.addWidget(self.commandLine, 4, 0, 1, 4)
+        self.mainLayout.addWidget(self.commandButton, 4, 4, 1, 1)
+        self.mainLayout.addWidget(self.logArea, 5, 0, 10, 5)
+        self.setLayout(self.mainLayout)
+
+    @property
+    def actor(self):
+        return self.mainTree.actor
+
+    def sendCommand(self):
+        [actor, cmdStr] = self.commandLine.text().split(' ', 1)
+
+        self.logArea.newLine('cmdIn=%s %s'%(actor, cmdStr))
+        self.actor.threadCmd(**dict(actor=actor, cmdStr=cmdStr))
+
---
+++ python/templateGUI/miniActor.py
@@ -0,0 +1,82 @@
+import logging
+
+import actorcore.ICC
+from actorcore.QThread import QThread, QMsg
+from PyQt5.QtCore import QObject, pyqtSignal, QTimer
+from PyQt5.QtCore import QThread as QtThread
+import queue
+from functools import partial
+
+
+class OneshotThread(QThread):
+    def __init__(self, actor):
+        QThread.__init__(self, actor, 'oneshot', timeout=0.2)
+        self.returnQueue = []
+
+        self.start()
+
+
+    def handleTimeout(self):
+        if self.returnQueue:
+            cmdVar = self.returnQueue[-1].get()
+            self.actor.dummyLab.setText('cmdOut=%s'%cmdVar.lastReply.canonical())
+            self.exit()
+        else:
+            pass
+
+
+    def callFunc(self, method, callTimeout=10.0, urgent=False, **argd):
+        """ send ourself a new message.
+
+        Args:
+            method: a function or bound method to call
+            **argd: the arguments to the method.
+        """
+        self.returnQueue.append(queue.Queue())
+        qmsg = QMsg(method, returnQueue=self.returnQueue[-1], **argd)
+        self.queue.put(qmsg, urgent=urgent)
+
+    def createSignal(self, msg):
+        return QTimer.singleShot(100, partial(self.actor.updateLog, msg))
+
+
+
+class OurActor(actorcore.ICC.ICC):
+    def __init__(self, name, productName=None, modelNames=None, configFile=None, logLevel=logging.INFO):
+        # This sets up the connections to/from the hub, the logger, and the twisted reactor.
+        #
+        modelNames = [] if modelNames is None else modelNames
+        actorcore.ICC.ICC.__init__(self, name,
+                                   productName=productName,
+                                   configFile=configFile,
+                                   modelNames=modelNames)
+
+        self.logger.setLevel(logLevel)
+        self.everConnected = False
+
+    def connectionMade(self):
+        if self.everConnected is False:
+            logging.info("alive!!!!")
+            self.everConnected = True
+
+    def disconnectActor(self):
+        self.shuttingDown = True
+
+    def threadCmd(self, **kwargs):
+        # self.cmdr.call(**kwargs)
+        cmdrThread = OneshotThread(self)
+        cmdrThread.callFunc(self.cmdr.call, **kwargs)
+
+    def updateLog(self, msg):
+        print (msg)
+        self.logArea.newLine(msg)
+
+
+def connectActor(modelNames):
+    theActor = OurActor('templategui',
+                        productName='templateGUI',
+                        modelNames=modelNames,
+                        logLevel=logging.DEBUG)
+
+    theActor.run(doReactor=False)
+    return theActor
